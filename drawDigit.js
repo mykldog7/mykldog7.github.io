3a1cba418736b0faed0ca5ab76e6d81c2e8f1aa6
mykldog7 <mykldog@gmail.com>
2015-07-16 00:15:15 +1200

    User classfied digits working.

--- drawDigit.js
+++ drawDigit.js
@@ -52,7 +52,7 @@ function redrawCanvas(){
 
 	g_context.strokeStyle = "#ffffff";
 	g_context.lineJoin = "round";
-	g_context.lineWidth = 15;
+	g_context.lineWidth = 23;
 			
 	for(var i=0; i < g_clickX.length; i++) {		
 		g_context.beginPath();
@@ -69,30 +69,78 @@ function redrawCanvas(){
 
 function applyNetworkToCanvas(){
 	//grab image data .. its 280*280*4. 
-	var imgData = g_context.getImageData(0, 0, g_context.canvas.width, g_context.canvas.height);
-	var vertSlices = $("#img-dim-height").val();		//default: 28	(rows)			//y
-	var horzSlices = $("#img-dim-width").val();			//default: 28	(columns)		//x
-	var innerSqrHeight = 10;
-	var innerSqrWidth = 10;
-	for(var i=0; i < vertSlices; i++){
-		for(var j=0; j < horzSlices; j++){
-			for(var x=0;x<innerSqrWidth;x++){
-				for(var y=0;y<innerSqrHeight;y++){
-					var rowLength = innerSqrWidth*vertSlices;
-					var currentPixelOffset = i * rowLength + 2											//y*rowlength + x
-					red=imgData.data[i+0];
-					green=imgData.data[i+1];
-					blue=imgData.data[i+2];
-					alpha=imgData.data[i+3];
-				
-				}
-			}
-		}
-	}
+	var imgDataObj = g_context.getImageData(0, 0, g_context.canvas.width, g_context.canvas.height);
+	
+	var imageParameters = {width: sliceCountX, height: sliceCountY};
+	
+	//use the old functions, first create an full image object. 
+	var retImage = reduceImageData(imgDataObj)
+	var singleImageArray= [];
+	var userImage = {};
+	userImage.width = sliceCountX;
+	userImage.height = sliceCountY;
+	userImage.values = retImage;
+	userImage.getPixelAt = function(x,y){ return this.values[y*this.width + x]; };	
+	
+	//setup labelarray
+	var result = classifyGivenImage(userImage);
+	var digitGuess = result.indexOf(1);
+	var userLabelArray= [];
+	userLabelArray[0] = digitGuess;
+	
+	//setupImage array;
+	singleImageArray[0] = userImage;
+	
+	//grab location to load new image and label
+	var placeHolderDiv = document.getElementById("processed-image-here");
+	
+	makeCanvasElements(placeHolderDiv, singleImageArray, imageParameters, "user-")
+	
+	//addLablesToCanvasesExt(parentELEM, labelArray, offset, lblColor1, lblColor2, imageList)
+	addLablesToCanvasesExt(placeHolderDiv, userLabelArray, 0, "orange", "black", "user-");			// in makeCanvas.js
+	
 }
 
+//globals used by other functions in this file. 
+var sliceCountX =28;
+var sliceCountY =28;
 
+//How many pixels in each slice in each dimension.
+var sliceX	= 10;
+var sliceY	= 10;
 
+//location in the original image. and location in the new image. 
+var xbig;
+var xsmall;
+
+var ybig;
+var ysmall;
+
+function reduceImageData(imgData){
+//Image is divided into chunks.. 28 in each dimension. Each of these chunks is a 10by10 square.
+// The percentage of these squares that are white is the greyscale value of the corresponding reduced image. 
+	//how many slices are made in the original image to reduce it to the require size for the network. 
+	
+	var processedImage = [];
+	for(ybig=0; ybig < sliceCountY; ybig++){
+		for(xbig=0; xbig < sliceCountX; xbig++){
+			var totalIntensitySlice = 0;
+			for(ysmall=0; ysmall < sliceY; ysmall++){
+				for(xsmall=0; xsmall < sliceX; xsmall++){
+					var rowLength = sliceCountX*sliceX;
+					var currentPixelOffset = ((ybig*sliceY*(rowLength)) + (xbig*sliceX) + (ysmall*(rowLength)) + (xsmall))*4; 	//*4 as each pixel is 4 elements. 							
+					
+					if(imgData.data[currentPixelOffset+0] > 0) {totalIntensitySlice += imgData.data[currentPixelOffset+0]};	//if the red channel is full then its white. // as I only let them draw white.
+					
+				}
+			}
+			//scale totalIntensitySlice from 0<10*10 (sliceX and sliceY) to between 0<x<255
+			var newPixVal = Math.floor((totalIntensitySlice/25500)*255);  // invert, multiply by (1/1275) and add 0.
+			processedImage.push(newPixVal);
+		}
+	}
+	return processedImage;
+}
 
 
 
--- makeCanvas.js
+++ makeCanvas.js
@@ -9,7 +9,8 @@
 //Loop over every image in the imagesArray. 
 // add generated canvas elements to the parentELEM. (blank it first.)
 // do this according to the imageParameters object.
-function makeCanvasElements(parentELEM, imagesArray, imageParameters){
+function makeCanvasElements(parentELEM, imagesArray, imageParameters, imageList){
+	if(imageList == undefined) {imageList = "image-";}
 	parentELEM.innerHTML = "";	//make sure the thing is empty. We want to overwrite. 
 	imagesArray.forEach(function(image,index){
 		var canvas = document.createElement("canvas");
@@ -18,7 +19,7 @@ function makeCanvasElements(parentELEM, imagesArray, imageParameters){
 		//generate pixel set
 		var imageData = graphicContext.createImageData(imageParameters.width, imageParameters.height);
 		//set canvas element attributes. 
-		canvas.id     = "image-"+index;
+		canvas.id     = imageList+index;
 		canvas.width  = imageParameters.width;
 		canvas.height = imageParameters.height;
 		canvas.className += "image-display col-sm-2";
@@ -34,9 +35,10 @@ function makeCanvasElements(parentELEM, imagesArray, imageParameters){
 	});
 }
 /*Add labels to canvas elements*/
-function addLablesToCanvases(parentELEM, labelArray){
+function addLablesToCanvases(parentELEM, labelArray, imageList){
+	if(imageList == undefined) {imageList = "image-";}
 	labelArray.forEach(function(label,index){
-		var cElem = document.getElementById("image-"+index);
+		var cElem = document.getElementById(imageList+index);
 		var gcontext = cElem.getContext("2d");
 		var xO = 21;		//postion and size of the small label...
 		var yO = 18;
@@ -51,9 +53,10 @@ function addLablesToCanvases(parentELEM, labelArray){
 }
 
 /*Add labels to canvas elements starting at offset. And specifying a colour.*/
-function addLablesToCanvasesExt(parentELEM, labelArray, offset, lblColor1, lblColor2){
+function addLablesToCanvasesExt(parentELEM, labelArray, offset, lblColor1, lblColor2, imageList){
+	if(imageList == undefined) {imageList = "image-";}
 	labelArray.forEach(function(label,index){
-		var cElem = document.getElementById("image-"+(index+offset));
+		var cElem = document.getElementById(imageList+(index+offset));
 		var gcontext = cElem.getContext("2d");
 		var xO = 21;		//postion and size of the small label...
 		var yO = 18;
